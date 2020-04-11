4233ad4cce1b94d7b9718acbacf771a91475432f
gurpreet <gurpreetsudan2094@gmail.com>
2020-04-11 19:36:06 +0530

    Added layout for single movie detail

---
+++ app/src/main/java/com/example/movieskotlin/single_movie_details/SingleMovie.kt
@@ -0,0 +1,13 @@
+package com.example.movieskotlin.single_movie_details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.movieskotlin.R
+
+class SingleMovie : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_movie)
+    }
+}
